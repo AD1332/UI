69f9417f5a0474398bdf1c3f95f08d808d274608
AD1332 <152504363+AD1332@users.noreply.github.com>
2023-12-06 11:30:21 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -o errexit
 
 pip install --upgrade Flask Werkzeug
+pip install pyaudio --global-option=build_ext --global-option="C:\Users\anjal\Downloads\portaudio\include"
 
 pip install --upgrade pip
 pip install -r requirements.txt
